179973ef26ca9475f47e2a61bbea71d9fef66459
ramusus <ramusus@gmail.com>
2016-08-02 00:23:04 +0300

    fixed migrations

--- odnoklassniki_groups/factories.py
+++ odnoklassniki_groups/factories.py
@@ -3,7 +3,8 @@ import factory
 import random
 
 class GroupFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = Group
-
     id = factory.Sequence(lambda n: n)
     members_count = random.randrange(0, 10000)
+
+    class Meta:
+        model = Group
--- odnoklassniki_groups/migrations/0001_initial.py
+++ odnoklassniki_groups/migrations/0001_initial.py
@@ -2,12 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import m2m_history.fields
 import annoying.fields
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('odnoklassniki_users', '0002_user_age'),
     ]
 
     operations = [
@@ -16,6 +18,8 @@ class Migration(migrations.Migration):
             fields=[
                 ('fetched', models.DateTimeField(db_index=True, null=True, verbose_name='\u041e\u0431\u043d\u043e\u0432\u043b\u0435\u043d\u043e', blank=True)),
                 ('id', models.BigIntegerField(help_text='\u0423\u043d\u0438\u043a\u0430\u043b\u044c\u043d\u044b\u0439 \u0438\u0434\u0435\u043d\u0442\u0438\u0444\u0438\u043a\u0430\u0442\u043e\u0440', serialize=False, verbose_name='ID', primary_key=True)),
+                ('albums_count', models.PositiveIntegerField(null=True)),
+                ('discussions_count', models.PositiveIntegerField(null=True)),
                 ('name', models.CharField(max_length=800)),
                 ('description', models.TextField()),
                 ('shortname', models.CharField(max_length=50)),
@@ -29,6 +33,7 @@ class Migration(migrations.Migration):
                 ('shop_visible_admin', models.NullBooleanField()),
                 ('shop_visible_public', models.NullBooleanField()),
                 ('attrs', annoying.fields.JSONField(null=True)),
+                ('users', m2m_history.fields.ManyToManyHistoryField(to='odnoklassniki_users.User')),
             ],
             options={
                 'verbose_name': 'Odnoklassniki group',
